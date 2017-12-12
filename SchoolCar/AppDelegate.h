2644626d79399491d6746e94f21e2d353f72950d
ZhouAaron <zj971021@163.com>
2017-12-12 23:45:46 +0800

    Initial Commit

---
+++ SchoolCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchoolCar
+//
+//  Created by 周杰 on 2017/12/12.
+//  Copyright © 2017年 周杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SchoolCar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchoolCar
+//
+//  Created by 周杰 on 2017/12/12.
+//  Copyright © 2017年 周杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
